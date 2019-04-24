140d6147c81294d937047fd54e5d73183baa4a92
Jan Werkhoven <jan.werkhoven@gmail.com>
2019-04-24 20:55:50 +1000

    Replace Instafeed with vanilla JS, fix Instagram feed, format JS

--- Gruntfile.js
+++ Gruntfile.js
@@ -208,7 +208,6 @@ module.exports = function(grunt) {
           "dist/assets/js/main.js": [
             "bower_components/jquery/dist/jquery.min.js",
             "bower_components/velocity/velocity.min.js",
-            "bower_components/instafeed.js/instafeed.min.js",
             "bower_components/countUp.js/dist/countUp.min.js",
             "src/js/main.js"
           ]
@@ -219,7 +218,6 @@ module.exports = function(grunt) {
           "dist/assets/js/main.min.js": [
             "bower_components/jquery/dist/jquery.min.js",
             "bower_components/velocity/velocity.min.js",
-            "bower_components/instafeed.js/instafeed.min.js",
             "bower_components/countUp.js/dist/countUp.min.js",
             "temp/js/main.min.js"
           ]
